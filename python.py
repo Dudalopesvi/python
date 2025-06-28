3c7fe9581430e7d0b069d89560a90faf5c6d7f2e
Dudalopesvi <lopesvieiraeduarda@gmail.com>
2025-06-28 14:49:58 -0300

    meu primeiro python

---
+++ python.py
@@ -0,0 +1,6 @@
+valor1 = float (input("Digite seu primeiro número"))
+valor2 = float (input("Digite seu segundo número") )
+valor3 = float (input("Digite seu terceiro número"))
+
+media = (valor1 + valor2 +valor3) /3
+print (f"Média das notas é : {media} ")
