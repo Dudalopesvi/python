089f43d452fe72d89a64c155e40046a2467d9f82
Eduarda Vieira <eduarda62629776@alunos.sc.senac.br>
2026-03-09 21:25:48 -0300

    feito

---
+++ imc.py
@@ -0,0 +1,18 @@
+nome = (input ("Qual seu nome? "))
+peso = float(input ("Qual sua peso? "))
+Altura = float(input ("Qual sua Altura? "))
+
+imc =  peso/(Altura ** 2) 
+print(f"IMC de {nome}: {imc:.2f} ")
+
+baixo_peso = imc <= 18.5
+Normal = (imc <= 18.5) and (imc < 25)
+acima_do_peso = (imc <= 25) and (imc < 30)
+obesidade = imc >= 30
+
+
+
+print("Abaixo do peso? ", baixo_peso)
+print("Normal" , Normal)
+print("Acima do peso? ", acima_do_peso)
+print("Obesidade?  ", obesidade)
--- primeiro_Programa_no_Senac.py
+++ primeiro_Programa_no_Senac.py
@@ -1,2 +1,24 @@
-aluna = input("Quaal seu nome?")
-print("Ola " + aluna)
+# aluna = input("Qual seu nome?")
+# print("Ola " + aluna)
+
+
+# lista_Compras = ["Arroz", "Frango","Feijão"]
+# print("Seus produtos até agora foram: " , lista_Compras)
+
+
+# lista_Compras.append("Pureza")
+# print("Seus produtos até agora foram: " , lista_Compras)
+
+# lista_Compras.remove("Pureza")
+
+# print("Seus produtos até agora foram: " , lista_Compras)
+
+# Aluno = [1,5,6] 
+
+
+# print(Aluno[0])
+
+aluna_Nota = {"id": 16 ,"nome": "Julia",  "Nota": 9.2 }
+
+print(aluna_Nota)
+print(aluna_Nota["nome"])
