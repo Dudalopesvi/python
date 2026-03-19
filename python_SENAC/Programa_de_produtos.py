ae27b17cbe9c1e9dac75c5daecaa44e2ecf24e5c
Eduarda Vieira <eduarda62629776@alunos.sc.senac.br>
2026-03-18 21:38:17 -0300

    salvando um exercicio de produto e estoque

---
+++ python_SENAC/Programa_de_produtos.py
@@ -0,0 +1,21 @@
+Catalogo = [
+
+a = ("Coca", 7.50,3),
+b =("Pureza", 6.50,4),
+c = ("Doritos G", 19.0,3),
+
+
+]
+
+
+print(Catalogo)
+print(type(Catalogo))
+print(type(Catalogo[0]))
+
+
+
+
+if Catalogo == int :
+    estoque = {}
+
+
